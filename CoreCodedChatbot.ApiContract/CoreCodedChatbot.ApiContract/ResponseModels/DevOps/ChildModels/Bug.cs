f01cb75b7e8ccf8699747a46d3d9158d7b4340f3
codedghost2 <codedghost2@gmail.com>
2020-01-04 15:22:03 +0000

    Adding Bug work types and extra fields to PBIs

--- CoreCodedChatbot.ApiContract/CoreCodedChatbot.ApiContract/ResponseModels/DevOps/ChildModels/Bug.cs
+++ CoreCodedChatbot.ApiContract/CoreCodedChatbot.ApiContract/ResponseModels/DevOps/ChildModels/Bug.cs
@@ -3,12 +3,13 @@ using System.Linq;
 
 namespace CoreCodedChatbot.ApiContract.ResponseModels.DevOps.ChildModels
 {
-    public class ProductBacklogItem
+    public class Bug
     {
         public string Title { get; set; }
         public string State { get; set; }
         public string AssignedTo { get; set; }
-        public string Description { get; set; }
+        public string ReproSteps { get; set; }
+        public string SystemInfo { get; set; }
         public string AcceptanceCriteria { get; set; }
         public List<Task> Tasks { get; set; }
 
