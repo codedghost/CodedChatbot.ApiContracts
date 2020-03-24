6ebf7851f022b4bc8a311ba07c7d6adc65123a32
codedghost2 <codedghost2@gmail.com>
2020-03-24 10:14:20 +0000

    Adding Save song info request

---
+++ CoreCodedChatbot.ApiContract/CoreCodedChatbot.ApiContract/RequestModels/Playlist/StoreSongInfoAndAddToDriveRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CoreCodedChatbot.ApiContract.RequestModels.Playlist
+{
+    public class StoreSongInfoAndAddToDriveRequest
+    {
+        public int SongRequestId { get; set; }
+        public int SongId { get; set; }
+        public string SongTitle { get; set; }
+        public string SongArtist { get; set; }
+        public string AlbumName { get; set; }
+        public decimal ChartVersion { get; set; }
+        public string CharterUsername { get; set; }
+        public DateTime UploadedDateTime { get; set; }
+        public DateTime UpdatedDateTime { get; set; }
+        public string DownloadUrl { get; set; }
+    }
+}
