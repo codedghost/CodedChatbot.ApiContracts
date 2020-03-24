7c4e2d6f7adcd069a3aa791eda3c8c601a69c4bc
codedghost2 <codedghost2@gmail.com>
2020-03-24 10:11:01 +0000

    Adding UnsavedSongRequests to Api Contract

---
+++ CoreCodedChatbot.ApiContract/CoreCodedChatbot.ApiContract/ResponseModels/Playlist/ChildModels/UnsavedSongRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace CoreCodedChatbot.ApiContract.ResponseModels.Playlist.ChildModels
+{
+    public class UnsavedSongRequest
+    {
+        public int SongRequestId { get; set; }
+        public string ArtistName { get; set; }
+        public string SongName { get; set; }
+        public string UnformattedString { get; set; }
+    }
+}
---
+++ CoreCodedChatbot.ApiContract/CoreCodedChatbot.ApiContract/ResponseModels/Playlist/GetUnsavedSongRequestsResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using CoreCodedChatbot.ApiContract.ResponseModels.Playlist.ChildModels;
+
+namespace CoreCodedChatbot.ApiContract.ResponseModels.Playlist
+{
+    public class GetUnsavedSongRequestsResponse
+    {
+        public List<UnsavedSongRequest> UnsavedSongRequests { get; set; }
+    }
+}
