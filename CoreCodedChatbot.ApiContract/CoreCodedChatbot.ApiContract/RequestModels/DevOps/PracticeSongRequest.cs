5f6b0720d6cc7592c33040ed98acc7bb85e699ef
codedghost2 <codedghost2@gmail.com>
2020-03-31 16:10:37 +0100

    Adding Practice Song Request contract

---
+++ CoreCodedChatbot.ApiContract/CoreCodedChatbot.ApiContract/RequestModels/DevOps/PracticeSongRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace CoreCodedChatbot.ApiContract.RequestModels.DevOps
+{
+    public class PracticeSongRequest
+    {
+        public string SongName { get; set; }
+        public string ExtraInformation { get; set; }
+        public string Username { get; set; }
+    }
+}
