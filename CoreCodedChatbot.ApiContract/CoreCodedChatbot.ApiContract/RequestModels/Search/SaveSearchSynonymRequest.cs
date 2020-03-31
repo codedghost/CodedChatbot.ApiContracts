78e35604e0ade1b1cf4f4629e99086193d85307f
codedghost2 <codedghost2@gmail.com>
2020-03-31 14:21:16 +0100

    SynonymRequest model

---
+++ CoreCodedChatbot.ApiContract/CoreCodedChatbot.ApiContract/RequestModels/Search/SaveSearchSynonymRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace CoreCodedChatbot.ApiContract.RequestModels.Search
+{
+    public class SaveSearchSynonymRequest
+    {
+        public string SearchSynonymRequest { get; set; }
+        public string Username { get; set; }
+    }
+}
