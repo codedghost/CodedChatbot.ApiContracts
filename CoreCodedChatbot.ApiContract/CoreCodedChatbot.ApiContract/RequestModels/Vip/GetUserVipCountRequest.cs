90894fbb2129bc304ebdc019bfee4a862a096f7c
codedghost2 <codedghost2@gmail.com>
2020-04-04 16:31:40 +0100

    Adding VipCount Request/Response Models

---
+++ CoreCodedChatbot.ApiContract/CoreCodedChatbot.ApiContract/RequestModels/Vip/GetUserVipCountRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace CoreCodedChatbot.ApiContract.RequestModels.Vip
+{
+    public class GetUserVipCountRequest
+    {
+        public string Username { get; set; }
+    }
+}
---
+++ CoreCodedChatbot.ApiContract/CoreCodedChatbot.ApiContract/ResponseModels/Vip/GetUserVipCountResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace CoreCodedChatbot.ApiContract.ResponseModels.Vip
+{
+    public class GetUserVipCountResponse
+    {
+        public int Vips { get; set; }
+    }
+}
