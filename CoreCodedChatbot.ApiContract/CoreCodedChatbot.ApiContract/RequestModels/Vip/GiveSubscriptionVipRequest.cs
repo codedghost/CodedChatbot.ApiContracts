471d7af604467297897b1bb9506d4ed896ce6936
codedghost2 <codedghost2@gmail.com>
2020-04-04 23:46:07 +0100

    Add GiveSubscriptionVipRequest model

---
+++ CoreCodedChatbot.ApiContract/CoreCodedChatbot.ApiContract/RequestModels/Vip/GiveSubscriptionVipRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace CoreCodedChatbot.ApiContract.RequestModels.Vip
+{
+    public class GiveSubscriptionVipRequest
+    {
+        public string Username { get; set; }
+        public int SubStreak { get; set; }
+    }
+}
