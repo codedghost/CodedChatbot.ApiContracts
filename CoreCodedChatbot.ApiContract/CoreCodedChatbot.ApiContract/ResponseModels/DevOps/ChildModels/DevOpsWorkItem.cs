13107c9288384e332c1aee2934de1619baaa532d
codedghost2 <codedghost2@gmail.com>
2020-04-04 11:50:11 +0100

    Adding tags list to Work Items in DevOps

--- CoreCodedChatbot.ApiContract/CoreCodedChatbot.ApiContract/ResponseModels/DevOps/ChildModels/DevOpsWorkItem.cs
+++ CoreCodedChatbot.ApiContract/CoreCodedChatbot.ApiContract/ResponseModels/DevOps/ChildModels/DevOpsWorkItem.cs
@@ -10,6 +10,7 @@ namespace CoreCodedChatbot.ApiContract.ResponseModels.DevOps.ChildModels
         public string State { get; set; }
         public string AssignedTo { get; set; }
         public string AcceptanceCriteria { get; set; }
+        public List<string> Tags { get; set; }
         public List<DevOpsTask> Tasks { get; set; }
 
         public int RemainingWork => Tasks.Sum(task => task.RemainingWork);
