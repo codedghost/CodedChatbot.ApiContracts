a3d48d65643563d628b58185a5da77fdf7c34a71
codedghost2 <codedghost2@gmail.com>
2020-04-04 21:16:01 +0100

    Adding better formatted PromoteSongResponse

---
+++ CoreCodedChatbot.ApiContract/CoreCodedChatbot.ApiContract/ResponseModels/Playlist/PromoteSongResponse.cs
@@ -0,0 +1,10 @@
+﻿using CoreCodedChatbot.ApiContract.Enums.Playlist;
+
+namespace CoreCodedChatbot.ApiContract.ResponseModels.Playlist
+{
+    public class PromoteSongResponse
+    {
+        public int PlaylistIndex { get; set; }
+        public PromoteRequestResult PromoteRequestResult { get; set; }
+    }
+}
