67c8ff257f544fc07acd37ef27de9cbf9b82874f
codedghost2 <codedghost2@gmail.com>
2020-04-05 18:12:04 +0100

    Bits and Donation Amount Request models

---
+++ CoreCodedChatbot.ApiContract/CoreCodedChatbot.ApiContract/RequestModels/Vip/UpdateDonatedAmountRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace CoreCodedChatbot.ApiContract.RequestModels.Vip
+{
+    public class UpdateDonatedAmountRequest
+    {
+        public string Username { get; set; }
+        public int AmountDonated { get; set; }
+    }
+}
---
+++ CoreCodedChatbot.ApiContract/CoreCodedChatbot.ApiContract/RequestModels/Vip/UpdateTotalBitsDroppedRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace CoreCodedChatbot.ApiContract.RequestModels.Vip
+{
+    public class UpdateTotalBitsDroppedRequest
+    {
+        public string Username { get; set; }
+        public int TotalBitsDropped { get; set; }
+    }
+}
