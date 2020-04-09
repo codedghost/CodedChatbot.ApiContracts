ba27b5ad93b58dc860da2eef84b2dcf8fa1e8388
codedghost2 <codedghost2@gmail.com>
2020-04-09 09:44:25 +0100

    Adding Transfer User Account request object

---
+++ CoreCodedChatbot.ApiContract/CoreCodedChatbot.ApiContract/RequestModels/Moderation/TransferUserAccountRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace CoreCodedChatbot.ApiContract.RequestModels.Moderation
+{
+    public class TransferUserAccountRequest
+    {
+        public string RequestingModerator { get; set; }
+        public string OldUsername { get; set; }
+        public string NewUsername { get; set; }
+    }
+}
