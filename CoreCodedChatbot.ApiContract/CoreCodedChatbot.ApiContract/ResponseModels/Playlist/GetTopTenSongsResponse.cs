9bafd15c1545ad8482541a03899216a33f5ae880
codedghost2 <codedghost2@gmail.com>
2020-04-09 09:16:24 +0100

    Adding TopTen Response object

---
+++ CoreCodedChatbot.ApiContract/CoreCodedChatbot.ApiContract/ResponseModels/Playlist/GetTopTenSongsResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using CoreCodedChatbot.ApiContract.ResponseModels.Playlist.ChildModels;
+
+namespace CoreCodedChatbot.ApiContract.ResponseModels.Playlist
+{
+    public class GetTopTenSongsResponse
+    {
+        public List<PlaylistItem> TopTenSongs { get; set; }
+    }
+}
