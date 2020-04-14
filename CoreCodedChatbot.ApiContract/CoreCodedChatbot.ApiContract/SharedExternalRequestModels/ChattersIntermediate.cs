cf18abbe1e5e7841e346a5960ea2fdc2ae65cdba
codedghost2 <codedghost2@gmail.com>
2020-04-14 13:52:12 +0100

    Adding TMI models

---
+++ CoreCodedChatbot.ApiContract/CoreCodedChatbot.ApiContract/SharedExternalRequestModels/ChattersIntermediate.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+
+namespace CoreCodedChatbot.ApiContract.SharedExternalRequestModels
+{
+    public class ChattersIntermediate
+    {
+        [JsonProperty("broadcaster")]
+        public string[] Broadcasters { get; set; }
+
+        [JsonProperty("moderators")]
+        public string[] Mods { get; set; }
+
+        [JsonProperty("staff")]
+        public string[] Staff { get; set; }
+
+        [JsonProperty("admins")]
+        public string[] Admins { get; set; }
+
+        [JsonProperty("global_mods")]
+        public string[] GlobalMods { get; set; }
+
+        [JsonProperty("vips")]
+        public string[] Vips { get; set; }
+
+        [JsonProperty("viewers")]
+        public string[] Viewers { get; set; }
+    }
+}
---
+++ CoreCodedChatbot.ApiContract/CoreCodedChatbot.ApiContract/SharedExternalRequestModels/TmiChattersIntermediate.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace CoreCodedChatbot.ApiContract.SharedExternalRequestModels
+{
+    public class TmiChattersIntermediate
+    {
+        [JsonProperty("_links")]
+        public object Links { get; set; }
+
+        [JsonProperty("chatter_count")]
+        public int ChatterCount { get; set; }
+
+        [JsonProperty("chatters")]
+        public ChattersIntermediate ChattersIntermediate { get; set; }
+    }
+}
