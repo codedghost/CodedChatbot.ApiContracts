36550a23c01098da2279e4c05c26c2b3aa7a86a4
codedghost2 <codedghost2@gmail.com>
2020-04-18 16:18:07 +0100

    Adding more information to BasicSongSearchResult

--- CoreCodedChatbot.ApiContract/CoreCodedChatbot.ApiContract/ResponseModels/Search/ChildModels/BasicSongSearchResult.cs
+++ CoreCodedChatbot.ApiContract/CoreCodedChatbot.ApiContract/ResponseModels/Search/ChildModels/BasicSongSearchResult.cs
@@ -1,10 +1,14 @@
-﻿namespace CoreCodedChatbot.ApiContract.ResponseModels.Search.ChildModels
+﻿using System.Collections.Generic;
+
+namespace CoreCodedChatbot.ApiContract.ResponseModels.Search.ChildModels
 {
     public class BasicSongSearchResult
     {
         public int SongId { get; set; }
         public string SongName { get; set; }
         public string ArtistName { get; set; }
-        public string DownloadUrl { get; set; }
+        public List<string> Instruments { get; set; }
+        public bool IsOfficial { get; set; }
+        public bool IsLinkDead { get; set; }
     }
 }
