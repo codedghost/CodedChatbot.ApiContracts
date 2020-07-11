ce24a53d028a9f249a7c16c9a3ed1783c6686bb4
codedghost2 <codedghost2@gmail.com>
2020-07-11 17:27:37 +0100

    Adding ChannelReward request models

---
+++ CoreCodedChatbot.ApiContract/CoreCodedChatbot.ApiContract/RequestModels/ChannelRewards/CreateOrUpdateChannelRewardRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CoreCodedChatbot.ApiContract.RequestModels.ChannelRewards
+{
+    public class CreateOrUpdateChannelRewardRequest
+    {
+        public Guid ChannelRewardId { get; set; }
+        public string RewardTitle { get; set; }
+        public string RewardDescription { get; set; }
+    }
+}
---
+++ CoreCodedChatbot.ApiContract/CoreCodedChatbot.ApiContract/RequestModels/ChannelRewards/StoreRewardRedemptionRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CoreCodedChatbot.ApiContract.RequestModels.ChannelRewards
+{
+    public class StoreRewardRedemptionRequest
+    {
+        public Guid ChannelRewardId { get; set; }
+        public string RedeemedBy { get; set; }
+        public DateTime RedeemedAt { get; set; }
+    }
+}
