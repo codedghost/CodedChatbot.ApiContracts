15e415e64f50dbeb254de86c7b10d04d7f4a79d0
codedghost2 <codedghost2@gmail.com>
2020-08-03 14:56:00 +0100

    Further changes

--- CoreCodedChatbot.ApiContract/CoreCodedChatbot.ApiContract/ResponseModels/Quotes/ChildModels/Quote.cs
+++ CoreCodedChatbot.ApiContract/CoreCodedChatbot.ApiContract/ResponseModels/Quotes/ChildModels/Quote.cs
@@ -1,4 +1,6 @@
-﻿namespace CoreCodedChatbot.ApiContract.ResponseModels.Quotes.ChildModels
+﻿using System;
+
+namespace CoreCodedChatbot.ApiContract.ResponseModels.Quotes.ChildModels
 {
     public class Quote
     {
@@ -6,6 +8,7 @@
         public string QuoteText { get; set; }
         public string CreatedBy { get; set; }
         public bool Disabled { get; set; }
-        public string DisabledBy { get; set; }
+        public string LastEditedBy { get; set; }
+        public DateTime EditedAt { get; set; }
     }
 }
