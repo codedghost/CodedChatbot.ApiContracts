27b3c87f2904a5122bb811928b483f223370a365
codedghost2 <codedghost2@gmail.com>
2020-09-13 15:11:09 +0100

    Adding UpdateSettingsRequest object

---
+++ CoreCodedChatbot.ApiContract/CoreCodedChatbot.ApiContract/RequestModels/Settings/UpdateSettingsRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreCodedChatbot.ApiContract.RequestModels.Settings
+{
+    public class UpdateSettingsRequest
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}
