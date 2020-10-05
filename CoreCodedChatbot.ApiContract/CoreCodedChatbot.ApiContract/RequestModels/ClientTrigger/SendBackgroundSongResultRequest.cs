814b99413b87e0166b4797bf9b0cf5664c2f8b06
codedghost2 <codedghost2@gmail.com>
2020-10-06 00:16:18 +0100

    Adding background song result request

---
+++ CoreCodedChatbot.ApiContract/CoreCodedChatbot.ApiContract/RequestModels/ClientTrigger/SendBackgroundSongResultRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace CoreCodedChatbot.ApiContract.RequestModels.ClientTrigger
+{
+    public class SendBackgroundSongResultRequest
+    {
+        public string Username { get; set; }
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string Url { get; set; }
+    }
+}
