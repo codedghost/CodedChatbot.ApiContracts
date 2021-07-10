c66a28b48db8a00ff07e3e49c7ab7f735c83ed01
codedghost2 <codedghost2@gmail.com>
2021-07-10 17:31:41 +0100

    Adding SongListHubPath to constants

--- CoreCodedChatbot.ApiContract/CoreCodedChatbot.ApiContract/SignalRHubModels/API/APIHubConstants.cs
+++ CoreCodedChatbot.ApiContract/CoreCodedChatbot.ApiContract/SignalRHubModels/API/APIHubConstants.cs
@@ -3,5 +3,6 @@
     public static class APIHubConstants
     {
         public const string BackgroundSongHubPath = "/BackgroundSongHub";
+        public const string SongListHubPath = "/SongList";
     }
 }
