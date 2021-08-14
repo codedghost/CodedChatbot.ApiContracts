116981b7207b27e19cd4e1133ebc41056bedbff3
codedghost2 <codedghost2@gmail.com>
2021-08-14 14:24:53 +0100

    Adding ClientId storage models

---
+++ CoreCodedChatbot.ApiContract/CoreCodedChatbot.ApiContract/RequestModels/ClientId/RemoveClientIdRequestModel.cs
@@ -0,0 +1,8 @@
+﻿namespace CoreCodedChatbot.ApiContract.RequestModels.ClientId
+{
+    public class RemoveClientIdRequestModel
+    {
+        public string HubType { get; set; }
+        public string ClientId { get; set; }
+    }
+}
---
+++ CoreCodedChatbot.ApiContract/CoreCodedChatbot.ApiContract/RequestModels/ClientId/SetClientIdRequestModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CoreCodedChatbot.ApiContract.RequestModels.ClientId
+{
+    public class SetClientIdRequestModel
+    {
+        public string HubType { get; set; }
+        public string Username { get; set; }
+        public string ClientId { get; set; }
+    }
+}
