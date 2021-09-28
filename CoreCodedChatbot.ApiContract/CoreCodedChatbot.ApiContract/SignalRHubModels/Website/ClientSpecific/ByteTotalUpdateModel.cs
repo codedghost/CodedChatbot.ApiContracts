9e7c9296168435853fd1c9fb94284034b4fc9985
codedghost2 <codedghost2@gmail.com>
2021-09-28 21:20:08 +0100

    Updating byte total update models

--- CoreCodedChatbot.ApiContract/CoreCodedChatbot.ApiContract/SignalRHubModels/Website/ClientSpecific/ByteTotalUpdateModel.cs
+++ CoreCodedChatbot.ApiContract/CoreCodedChatbot.ApiContract/SignalRHubModels/Website/ClientSpecific/ByteTotalUpdateModel.cs
@@ -1,7 +1,16 @@
-﻿namespace CoreCodedChatbot.ApiContract.SignalRHubModels.Website.ClientSpecific
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace CoreCodedChatbot.ApiContract.SignalRHubModels.Website.ClientSpecific
 {
-    public class ByteTotalUpdateModel : ClientSpecificHubRequest
+    public class ByteTotalUpdateModel : AuthorizedHubRequest
+    {
+        private IEnumerable<ByteTotalUserModel> UserUpdates { get; set; }
+    }
+
+    public class ByteTotalUserModel
     {
         public string TotalBytes { get; set; }
+        public string ClientId { get; set; }
     }
 }
