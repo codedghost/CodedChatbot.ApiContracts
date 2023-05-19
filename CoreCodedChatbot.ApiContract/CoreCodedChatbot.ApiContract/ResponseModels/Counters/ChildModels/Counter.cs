e24460ed08c4f4a003368f63cc1e47fd74d15b64
codedghost2 <codedghost2@gmail.com>
2023-05-19 14:04:21 +0100

    Removing CounterId

--- CoreCodedChatbot.ApiContract/CoreCodedChatbot.ApiContract/ResponseModels/Counters/ChildModels/Counter.cs
+++ CoreCodedChatbot.ApiContract/CoreCodedChatbot.ApiContract/ResponseModels/Counters/ChildModels/Counter.cs
@@ -4,7 +4,6 @@ namespace CoreCodedChatbot.ApiContract.ResponseModels.Counters.ChildModels
 {
     public class Counter
     {
-        public Guid CounterId { get; set; }
         public string CounterName { get; set; } = default!;
         public string CounterPreText { get; set; } = default!;
         public int CounterValue { get; set; }
