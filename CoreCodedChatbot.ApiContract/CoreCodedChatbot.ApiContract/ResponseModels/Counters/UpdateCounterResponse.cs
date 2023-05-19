358b35419475e17b7162e02561fb640607c1be70
codedghost2 <codedghost2@gmail.com>
2023-05-19 14:24:12 +0100

    Adding update response model

---
+++ CoreCodedChatbot.ApiContract/CoreCodedChatbot.ApiContract/ResponseModels/Counters/UpdateCounterResponse.cs
@@ -0,0 +1,9 @@
+﻿using CoreCodedChatbot.ApiContract.ResponseModels.Counters.ChildModels;
+
+namespace CoreCodedChatbot.ApiContract.ResponseModels.Counters
+{
+    public class UpdateCounterResponse
+    {
+        Counter Counter { get; set; }
+    }
+}
