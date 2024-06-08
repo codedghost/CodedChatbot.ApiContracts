002a8655841c3d2431220ff5ed31bb9325ef0019
Sean McNeilly <sean.mcneilly@hotmail.co.uk>
2024-06-08 16:39:03 +0100

    Adding SubmissionRequest model

---
+++ CoreCodedChatbot.ApiContract/CoreCodedChatbot.ApiContract/RequestModels/Ylyl/YlylSubmissionRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace CoreCodedChatbot.ApiContract.RequestModels.Ylyl;
+
+public class YlylSubmissionRequest
+{
+    public ulong ChannelId { get; set; }
+    public ulong MessageId { get; set; }
+    public int TotalImages { get; set; }
+    public int TotalVideos { get; set; }
+}
